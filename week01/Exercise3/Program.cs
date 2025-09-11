4678964b7b034fd41c44c2f74dae108a4d2ee92c
Dreyes1296 <edsonreyes555@gmail.com>
2025-09-10 19:35:16 -0600

    Excercise 3 done

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        // 1. Generate a random number from 1 to 100.
+
+        Random randomGenerator = new Random();
+        
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        int guess = 0;
+
+        // 2. Add a loop as long as the guess does not match the magic number.
+        while (guess != magicNumber)
+        {
+            // Ask the user for their guess.
+            Console.Write(" Hi there! What is your guess? ");
+            
+            // Read the user's input from the console and convert it to an integer.
+            string guessInput = Console.ReadLine();
+            guess = int.Parse(guessInput);
+
+            // 3. Use an if statement to determine if the guess is higher, lower, or correct.
+            if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher!");
+            }
+            else if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower!");
+            }
+            else
+            {
+                // The message when the guess matches the magic number.
+                Console.WriteLine("You guessed it correctly!");
+            }
+        }
     }
 }
